6a5d9835c54f8ce79e8e01dba1ba9a416d246e42
Ville Koskela <vkoskela@gmail.com>
2021-10-23 20:26:19 +0300

    w9 4/5 done

--- w9/own/main.py
+++ w9/own/main.py
@@ -1,33 +1,40 @@
 import converter
 import tkinter as tk
 
-min_width = 40
-min_height = 20
+min_width = 400
+min_height = 200
 
 
 class GUI:
 
     # https://stackoverflow.com/questions/45441885/how-can-i-create-a-dropdown-menu-from-a-list-in-tkinter
     SYSTEMS = [
-      "bin",
-      "oct",
-      "dec",
-      "hex"
+        "bin",
+        "oct",
+        "dec",
+        "hex"
     ]
 
     # initialize object for GUI
     def __init__(self, master):
         self.master = master
-        self.options = tk.StringVar(master)
-        self.options.set(self.SYSTEMS[2])
+        self.input_format = tk.StringVar(master)
+        self.input_format.set(self.SYSTEMS[2])
+        self.output_format = tk.StringVar(master)
+        self.output_format.set(self.SYSTEMS[2])
         self.title_label = tk.Label(text="Number converter", font=('bold', 20))
-        self.input_menu = tk.OptionMenu(master, self.options, *self.SYSTEMS)
+        self.input_menu_label = tk.Label(text="Input format: ")
+        self.input_menu = tk.OptionMenu(
+            master, self.input_format, *self.SYSTEMS)
         self.input_label = tk.Label(text="Input:")
         self.input = tk.Entry()
+        self.output_menu_label = tk.Label(text="Output format: ")
+        self.output_menu = tk.OptionMenu(
+            master, self.output_format, *self.SYSTEMS)
         self.output_label = tk.Label(text="=>")
         self.output = tk.Label()
         self.convert_button = tk.Button(
-            text="convert", command=self.test_handler)
+            text="convert", command=self.conversion_handler)
         self.handle_layout()
 
     # handle layout
@@ -35,20 +42,38 @@ class GUI:
         self.master.rowconfigure(5, weight=1)
         self.master.columnconfigure(4, minsize=100, weight=1)
         self.title_label.grid(row=0, column=0, columnspan=4, sticky="nsew")
-        self.input_menu.grid(row=1, column=0)
+        self.input_menu_label.grid(row=1, column=0)
+        self.input_menu.grid(row=1, column=1)
+        self.output_menu_label.grid(row=1, column=2)
+        self.output_menu.grid(row=1, column=3)
         self.input_label.grid(row=2, column=0)
         self.input.grid(row=2, column=1)
         self.output_label.grid(row=2, column=2)
         self.output.grid(row=2, column=3)
-        self.convert_button.grid(row=3, column=0, columnspan=1)
+        self.convert_button.grid(row=3, column=0, columnspan=4)
 
     # event handlers for buttons etc.
-    def test_handler(self):
+    def conversion_handler(self):
         value = self.input.get()
-        foo = converter.from_decimal(value, 2)
-        self.output["text"] = foo
+        input_type = self.get_format(self.input_format)
+        output_type = self.get_format(self.output_format)
+        if input_type != "dec":
+            value = converter.to_decimal(value, input_type)
+        if output_type != "dec":
+            value = converter.from_decimal(value, output_type)
+        self.output["text"] = value
 
     # helper functions
+    def get_format(self, format):
+        f = format.get()
+        if f == "bin":
+            return 2
+        if f == "oct":
+            return 8
+        if f == "dec":
+            return 10
+        if f == "hex":
+            return 16
 
 
 if __name__ == '__main__':
