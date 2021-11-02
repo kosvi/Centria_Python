b4c1bd3137efc19883283c55cf404469cd70e9ca
Ville Koskela <vkoskela@gmail.com>
2021-11-02 20:07:52 +0200

    fixed cases where n<1

--- w6/e4.py
+++ w6/e4.py
@@ -2,8 +2,16 @@
 
 
 def factorial(n):
-  if n==1:
+  if n<0:
+    return -1
+  if n<=1:
     return 1
   else:
     return n*factorial(n-1)
 
+
+if __name__ == "__main__":
+  print(factorial(-1))
+  print(factorial(0))
+  print(factorial(2))
+  print(factorial(10))
