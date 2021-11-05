cfbd9a656f5c197292e6311efd000c4d51f41caa
Ville Koskela <vkoskela@gmail.com>
2021-11-05 20:52:29 +0200

    fixed tests

--- w10/test_countries.py
+++ w10/test_countries.py
@@ -1,6 +1,9 @@
+import os
 from countries import Countries
 
 def test_find_country():
-  c = Countries("countries.txt")
+  # https://stackoverflow.com/questions/5137497/find-the-current-directory-and-files-directory
+  dirpath = os.path.dirname(os.path.realpath(__file__))
+  c = Countries(f"{dirpath}/countries.txt")
   assert c.find_country("Finland") == "5508198"
 
